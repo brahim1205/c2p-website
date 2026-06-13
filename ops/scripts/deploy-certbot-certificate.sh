1ea766a71d03923ff538169a781a7b2fa85f2182
brahim1205 <sadiocherif11@gmail.com>
2026-06-13 12:43:02 +0000

    Fiabiliser le renouvellement TLS

--- ops/scripts/deploy-certbot-certificate.sh
+++ ops/scripts/deploy-certbot-certificate.sh
@@ -5,9 +5,9 @@ APP_DIR="${APP_DIR:-/opt/c2p}"
 LINEAGE="${RENEWED_LINEAGE:-/etc/letsencrypt/live/c2p.sn}"
 CERT_DIR="${APP_DIR}/ops/nginx/certs"
 
-install -d -m 0750 "${CERT_DIR}"
-install -m 0644 "${LINEAGE}/fullchain.pem" "${CERT_DIR}/fullchain.pem.new"
-install -m 0600 "${LINEAGE}/privkey.pem" "${CERT_DIR}/privkey.pem.new"
+install -d -o root -g 101 -m 0750 "${CERT_DIR}"
+install -o root -g 101 -m 0644 "${LINEAGE}/fullchain.pem" "${CERT_DIR}/fullchain.pem.new"
+install -o root -g 101 -m 0640 "${LINEAGE}/privkey.pem" "${CERT_DIR}/privkey.pem.new"
 mv -f "${CERT_DIR}/fullchain.pem.new" "${CERT_DIR}/fullchain.pem"
 mv -f "${CERT_DIR}/privkey.pem.new" "${CERT_DIR}/privkey.pem"
 
